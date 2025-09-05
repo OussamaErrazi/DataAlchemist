1d8699bd324b53ce9376b60799fee10cc7cdc0fa
ERRAZI <errazi111@gmail.com>
2025-09-05 16:57:31 +0100

    created transformation service

---
+++ transformation-service/src/test/java/com/DataAlchemist/transformation_service/TransformationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DataAlchemist.transformation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
