444e1d266ab61fd17c72246d69a8dae539773f26
ERRAZI <errazi111@gmail.com>
2025-12-09 17:53:39 +0100

    added reader service constants

---
+++ reader-service/src/main/java/com/DataAlchemist/reader_service/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package com.DataAlchemist.reader_service.constants;
+
+public interface AppConstants {
+    int SAMPLE_LINES = 10;
+    String DATA_STREAM_RESPONSE_TOPIC = "data-stream-response-topic";
+    String DATA_STREAM_REQUEST_TOPIC = "data-stream-request-topic";
+    String DATA_RETRIEVING_WORKERS_GROUP = "data-retrieving-workers";
+    String CSV_LINE_SPLITTER = ",";
+}
